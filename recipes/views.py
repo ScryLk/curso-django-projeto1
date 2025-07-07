f42cfcd22005d933a928f922fdd3e0c619cd5660
Lucas Augusto Kepler <lucaskepler991@gmail.com>
2025-07-07 14:20:12 -0300

    new commit

--- recipes/views.py
+++ recipes/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_list_or_404, get_object_or_404
 from django.http import Http404
 from .models import Recipe
 from utils.recipes.factory import make_recipe
+from django.db.models import Q
 
 def home(request):
     recipes = Recipe.objects.filter(
@@ -27,3 +28,23 @@ def recipe(request, id):
   recipe = get_object_or_404(Recipe, pk=id, is_published=True,)
   return render(request, "recipes/pages/recipe-view.html", 
       context={'recipe': recipe, 'is_detail_page': True})
+
+def search(request):
+    search_term = request.GET.get('q', '').strip()
+    
+    if not search_term:
+        raise Http404()
+    
+    recipes = Recipe.objects.filter(
+        Q(
+            Q(title__icontains=search_term) |
+            Q(description__icontains=search_term),
+        ),
+        is_published=True
+    ).order_by('-id')
+    
+    return render(request, 'recipes/pages/search.html', {
+        'page_title': f"Search for '{search_term}' | ",
+        'search_term': search_term,
+        'recipes': recipes,
+    })
